b55524c90444e3c14427f79381050f7d044037d2
vladimirfomene <vladimirfomene@gmail.com>
2019-12-26 12:31:27 +0100

    implement get mime from file extension

--- src/mime.c
+++ src/mime.c
@@ -1,6 +1,6 @@
 #include <string.h>
 #include <ctype.h>
-
+#include "mime.h"
 
 char *strlower(char *str){
 	
@@ -14,8 +14,24 @@ char *strlower(char *str){
 
 char *get_mime_type(char *filename){
 
+		char* dot_idx = strrchr(filename, '.');
 
+		if(dot_idx == NULL){
+			return "application/octet-stream";
+		}
 
+		char* ext = strlower(++dot_idx);
 
+		//do file extension comparison to assign mime type
+		if(strcmp(ext, "jpeg") == 0) return "image/jpeg";
+		if(strcmp(ext, "gif") == 0) return "image/gif";
+		if(strcmp(ext, "png") == 0) return "image/png";
+		if(strcmp(ext, "js") == 0) return "application/javascript";
+		if(strcmp(ext, "json") == 0) return "application/json";
+		if(strcmp(ext, "css") == 0) return "text/css";
+		if(strcmp(ext, "html") == 0) return "text/html";
+		if(strcmp(ext, "txt") == 0) return "text/plain");
+		
+		return "application/octet-stream";
 
 }
