c9ab8c8258a7d56fff305d13e7f335786af1b2ae
vladimirfomene <vladimirfomene@gmail.com>
2020-01-02 16:06:01 +0100

    add a request info struct

--- src/net.h
+++ src/net.h
@@ -1,6 +1,12 @@
 #ifndef _NET_H
 #define _NET_H
 
+struct request_info {
+	char *method;
+	char *filepath;
+	char *protocol;
+};
+
 int get_listener_socket(char *port);
 void *get_in_addr(struct sockaddr *sa);
 
