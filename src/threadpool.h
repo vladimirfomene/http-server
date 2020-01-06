8d8b81894e65bb0e75adc5eb2fce10f15f0daf9b
vladimirfomene <vladimirfomene@gmail.com>
2020-01-06 16:06:46 +0100

    implement header for threadpool

---
+++ src/threadpool.h
@@ -0,0 +1,33 @@
+#ifndef __TPOOL_H__
+#define __TPOOL_H__
+
+#include <pthread.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef void (*thread_func_t)(void *arg);
+
+typedef struct tpool_work {
+    thread_func_t      func;
+    void              *arg;
+    struct tpool_work *next;
+} tpool_work_t;
+
+typedef struct tpool {
+    tpool_work_t    *work_first;
+    tpool_work_t    *work_last;
+    pthread_mutex_t  work_mutex;
+    pthread_cond_t   work_cond;
+    pthread_cond_t   working_cond;
+    size_t           working_cnt;
+    size_t           thread_cnt;
+    bool             stop;
+} tpool_t;
+
+
+tpool_t *tpool_create(size_t num);
+void tpool_destroy(tpool_t *tm);
+bool tpool_add_work(tpool_t *tm, thread_func_t func, void *arg);
+void tpool_wait(tpool_t *tm);
+
+#endif
